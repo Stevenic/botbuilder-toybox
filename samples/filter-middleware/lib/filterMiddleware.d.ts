2200191d3779dffa07020d00f73aadc104193347
ryanvolum <ryanvolum@gmail.com>
2018-02-19 22:02:35 -0800

    moved filter-middleware package

--- samples/filter-middleware/lib/filterMiddleware.d.ts
+++
@@ -1,2 +0,0 @@
-import { Middleware, MiddlewareSet } from 'botbuilder';
-export declare function filterMiddleware(predicate: (context: BotContext) => boolean, ...middlewares: Middleware[]): MiddlewareSet;
