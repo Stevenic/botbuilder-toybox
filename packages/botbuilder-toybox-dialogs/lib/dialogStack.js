63378a4927a79b5774ca5f9a0d7e973e7b0588cf
Steven Ickman <stevenic@microsoft.com>
2018-03-21 00:12:40 -0700

    Deprecating dialogs & prompts

--- packages/botbuilder-toybox-dialogs/lib/dialogStack.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * @module botbuilder-toybox-dialogs
- */
-/** Licensed under the MIT License. */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=dialogStack.js.map
--- packages/botbuilder-toybox-dialogs/lib/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./dialogSet"));
-__export(require("./waterfall"));
-__export(require("./prompts/promptSet"));
-//# sourceMappingURL=index.js.map
