c68116b8d806b3e521dd142c97932bab21ec61dd
Steven Ickman <stevenic@microsoft.com>
2018-09-23 23:20:26 -0700

    Deleted botbuilder-memories

--- packages/botbuilder-toybox-memories/src/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @module botbuilder-toybox
- */
-/** Licensed under the MIT License. */
-export * from './botStateFragment';
-export * from './manageScopes';
-export * from './memoryFragment';
-export * from './memoryScope';
-export * from './scopeAccessor';
